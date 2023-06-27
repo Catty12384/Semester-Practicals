c4eb65588c8bb9caa7ef9315d9a309627d3be12f
Sherin Joys Catherina <120629225+Catty12384@users.noreply.github.com>
2023-06-27 08:41:16 +0530

    Initial

---
+++ SemPractical/src/main/java/com/semester/SemPractical/SemPracticalApplication.java
@@ -0,0 +1,13 @@
+package com.semester.SemPractical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemPracticalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemPracticalApplication.class, args);
+	}
+
+}
---
+++ SemPractical/src/test/java/com/semester/SemPractical/SemPracticalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semester.SemPractical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemPracticalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
